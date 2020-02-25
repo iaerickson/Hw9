9b434855cd852649506004ad31fe2a89f67bc4ce
Ian Erickson <ierickson25@gmail.com>
2020-02-25 14:58:00 -0600

    initial commit

---
+++ Starter/index.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const axios = require("axios");
+
+const username;
+const color;
+
+
+
+
+// const questions = [
+//   `What is your git ${username}?`,
+//   `Which ${color} do you prefer?`
+// ];
+
+
+
+function writeToFile(fileName, data) {
+
+    
+    // fs.writeFile("log.txt", process.argv[2], function (err) {
+    // if (err) {
+    //     return console.log(err);
+    //   }
+    //   console.log("Success!");
+    // });
+}
+
+function init() {}
+init();
