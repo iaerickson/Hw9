e4293f0cf72d44c824698923f266d012200172ad
Ian Erickson <ierickson25@gmail.com>
2020-02-26 01:09:44 -0600

    added axios, updated json, and pseudo code

--- Starter/index.js
+++ Starter/index.js
@@ -1,24 +1,55 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
+const axios = require("axios");
 const generateHTML = require("./generateHTML");
 
-// const questions = [
-//   `What is your git ${username}?`,
-//   `Which ${color} do you prefer?`
-// ];
+//might have to change these to inquirer to collect user info and color
+//once user name is collected, make axios call to git to collect git info
+//Name
+//Bio Image
+//User Location
+//Git Link
+//public repositories, followers, GitHub stars and following count.
 
+//all this info stored in user object
+//distributed to html variable
+//once html is generated, saved into pdf file
+
+const questions = [
+  `What is your git ${username}?`,
+  `Which ${color} do you prefer?`
+];
+
+inquirer
+  .prompt({
+    message: "What is your GitHub Username?",
+    name: "username"
+  })
+  .then(function({ username }) {
+    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+
+    axios.get(queryUrl).then(function(response) {
+      const repoNames = res.data.map(function(repo) {
+        return repo.name;
+      });
+      //const repoNamesStr = repoNames.join("\n");
+    });
+  });
+
+//function will write to "profile.pdf" and be sent the html page generated.
 function writeToFile(fileName, data) {
-  // fs.writeFile("log.txt", process.argv[2], function (err) {
-  // if (err) {
-  //     return console.log(err);
-  //   }
-  //   console.log("Success!");
-  // });
+  fs.writeFile(fileName, data, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Success!");
+  });
 }
 
 function init() {
   console.log("Init Test");
-  generateHTML();
+  //const html = generateHTML();
+  //writeToFile("profile.pdf", html);
 }
 init();
